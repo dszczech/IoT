6c1b9cba9ea4b5c46216292ca6598c6660682f2d
Dominik Szczechowiak <dszczechowiak@edu.cdv.pl>
2021-03-28 18:37:03 +0200

    1
    1

---
+++ IoT 1/PeopleApp/Controllers/PeopleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var people = new List<Models.Person>
+            {
+                new Models.Person
+                {
+                FirstName = "Dominik",
+                LastName = "Szczechowiak",
+                Id = 1
+                }
+            };
+
+            return Ok(people);
+        }
+    }
+}
---
+++ IoT 1/PeopleApp/PeopleDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PeopleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleApp
+{
+    public class PeopleDb : DbContext
+    {
+        public PeopleDb(DbContextOptions<PeopleDb> options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> People { get; set; }
+    }
+}
